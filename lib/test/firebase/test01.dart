b873cbfd31424b42d6f8103d69d7afc8f3b4b8a8
= <=>
2020-11-11 12:03:47 +0900

    Firebaseのテストコード作成。 ユーザ登録をメアド・パスで出来るようにしている。

--- lib/test/firebase/test01.dart
+++ lib/test/firebase/test01.dart
@@ -1,258 +1,93 @@
-import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
+    Firebase.initializeApp(); // news
     return MaterialApp(
-      title: 'かしかりメモ',
-      home: List(),
+      debugShowCheckedModeBanner: true,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyAuthPage(),
     );
   }
 }
 
-class List extends StatefulWidget {
+class MyAuthPage extends StatefulWidget {
   @override
-  _MyList createState() => _MyList();
+  _MyAuthPageState createState() => _MyAuthPageState();
 }
 
-class _MyList extends State<List> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("リスト画面"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance
-                .collection('kasikari-memo')
-                .snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (!snapshot.hasData) return const Text('Loading...');
-              return ListView.builder(
-                itemCount: snapshot.data.docs.length,
-                padding: const EdgeInsets.only(top: 10.0),
-                itemBuilder: (context, index) =>
-                    _buildListItem(context, snapshot.data.docs[index]),
-              );
-            }),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            print('新規作成ボタンを押しました');
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  settings: const RouteSettings(name: "/new"),
-                  //新規作成ボタンの修正
-                  builder: (BuildContext context) => InputForm(null)),
-            );
-          }),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: const Icon(Icons.android),
-            title: Text(
-                "【 ${document['borrowOrLend'] == "lend" ? "貸" : "借"} 】" +
-                    document['stuff']),
-            subtitle: Text('期限 ： ' +
-                document['date'].toString().substring(0, 10) +
-                "\n相手 ： " +
-                document['user']),
-          ),
-          ButtonBarTheme(
-            data: const ButtonBarThemeData(alignment: MainAxisAlignment.center),
-            child: ButtonBar(
-              children: <Widget>[
-                FlatButton(
-                    child: const Text('へんしゅう'),
-                    onPressed: () {
-                      print('編集ボタンを押しました');
-                      //編集ボタンの処理追加
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            settings: const RouteSettings(name: '/edit'),
-                            builder: (BuildContext context) =>
-                                InputForm(document)),
-                      );
-                    }),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class InputForm extends StatefulWidget {
-  //引数の追加
-  InputForm(this.document);
-  final DocumentSnapshot document;
-
-  @override
-  _MyInputFormState createState() => _MyInputFormState();
-}
-
-class _FormData {
-  String borrowOrLend = 'borrow';
-  String user;
-  String stuff;
-  DateTime date = DateTime.now();
-}
-
-class _MyInputFormState extends State<InputForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final _FormData _data = _FormData();
-
-  void _setLendOrRent(String value) {
-    setState(() {
-      _data.borrowOrLend = value;
-    });
-  }
-
-  Future<DateTime> _selectTime(BuildContext context) {
-    return showDatePicker(
-        context: context,
-        initialDate: _data.date,
-        firstDate: DateTime(_data.date.year - 2),
-        lastDate: DateTime(_data.date.year + 2));
-  }
-
+class _MyAuthPageState extends State<MyAuthPage> {
+  // 入力されたメールアドレス
+  String newUserEmail = '';
+  // 入力されたパスワード
+  String newUserPassword = '';
+  // 登録・ログインに関する情報を表示
+  String infoText = '';
   @override
   Widget build(BuildContext context) {
-    //編集データの作成
-    DocumentReference _mainReference;
-    _mainReference =
-        FirebaseFirestore.instance.collection('kasikari-memo').doc();
-    if (widget.document != null) {
-      //引数で渡したデータがあるかどうか
-      if (_data.user == null && _data.stuff == null) {
-        _data.borrowOrLend = widget.document['borrowOrLend'];
-        _data.user = widget.document['user'];
-        _data.stuff = widget.document['stuff'];
-        _data.date = widget.document['date'];
-      }
-      _mainReference = FirebaseFirestore.instance
-          .collection('kasikari-memo')
-          .doc(widget.document.id);
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('かしかり入力'),
-        actions: <Widget>[
-          IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: () {
-                print('保存ボタンを押しました');
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  _mainReference.set({
-                    'borrowOrLend': _data.borrowOrLend,
-                    'user': _data.user,
-                    'stuff': _data.stuff,
-                    'date': _data.date
-                  });
-                  Navigator.pop(context);
-                }
-              }),
-          IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              print("削除ボタンを押しました");
-            },
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            padding: const EdgeInsets.all(20.0),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
             children: <Widget>[
-              RadioListTile(
-                value: "borrow",
-                groupValue: _data.borrowOrLend,
-                title: Text("借りた"),
+              TextFormField(
+                // テキスト入力のラベルを設定
+                decoration: const InputDecoration(labelText: 'メールアドレス'),
                 onChanged: (String value) {
-                  print("借りたをタッチしました");
-                  _setLendOrRent(value);
+                  setState(() {
+                    newUserEmail = value;
+                  });
                 },
-              ),
-              RadioListTile(
-                  value: 'lend',
-                  groupValue: _data.borrowOrLend,
-                  title: const Text("貸した"),
-                  onChanged: (String value) {
-                    print('貸したをタッチしました');
-                    _setLendOrRent(value);
-                  }),
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: '相手の名前',
-                  labelText: 'Name',
-                ),
-                onSaved: (String value) {
-                  _data.user = value;
-                },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return '名前は必須入力項目です';
-                  }
-                },
-                initialValue: _data.user,
               ),
               TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.business_center),
-                  hintText: '借りたもの、貸したもの',
-                  labelText: 'loan',
-                ),
-                onSaved: (String value) {
-                  _data.stuff = value;
+                decoration: const InputDecoration(labelText: 'パスワード（６文字以上）'),
+                // パスワードが見えないようにする
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
                 },
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return '借りたもの、貸したものは必須入力項目です';
-                  }
-                },
-                initialValue: _data.stuff,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Text("締め切り日：${_data.date.toString().substring(0, 10)}"),
               ),
               RaisedButton(
-                child: const Text("締め切り日変更"),
-                onPressed: () {
-                  print("締め切り日変更をタッチしました");
-                  _selectTime(context).then((time) {
-                    if (time != null && time != _data.date) {
-                      setState(() {
-                        _data.date = time;
-                      });
-                    }
-                  });
+                onPressed: () async {
+                  try {
+                    // メール/パスワードでユーザー登録
+                    final auth = FirebaseAuth.instance;
+                    final result =
+                    await auth.createUserWithEmailAndPassword(
+                      email: newUserEmail,
+                      password: newUserPassword,
+                    );
+                    // 登録したユーザー情報
+                    final user = result.user;
+                    setState(() {
+                      infoText = '登録OK：${user.email}';
+                    });
+                  // ignore: avoid_catches_without_on_clauses
+                  } catch (e) {
+                    // 登録に失敗した場合
+                    setState(() {
+                      infoText = '登録NG：${e.message}';
+                    });
+                  }
                 },
+                child: const Text('ユーザー登録'),
               ),
+              Text(infoText)
             ],
           ),
         ),
@@ -260,5 +95,3 @@ class _MyInputFormState extends State<InputForm> {
     );
   }
 }
-
-// ToDo 参考書通りに足す
